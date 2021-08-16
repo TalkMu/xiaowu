c5de33f11eeed35e3aaa587ee2b8f9441e26ed76
yangzhuang <koolss@koolss.com>
2021-08-17 01:21:36 +0800

    完善tabBar

---
+++ lib/page/my/MyPage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyPage extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: Text("我的"),
+    );
+  }
+}
--- lib/page/tab_bar/TabBarPage.dart
+++ lib/page/tab_bar/TabBarPage.dart
@@ -2,6 +2,9 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:xiaowu/page/home/HomePage.dart';
+import 'package:xiaowu/page/message/MessagePage.dart';
+import 'package:xiaowu/page/my/MyPage.dart';
+import 'package:xiaowu/page/robot/RobotPage.dart';
 import 'package:xiaowu/page/service/ServicePage.dart';
 
 class TabBarPage extends StatefulWidget {
@@ -12,7 +15,7 @@ class TabBarPage extends StatefulWidget {
 class TabBarPageState extends State<TabBarPage> {
   // 当前页面标识
   int _currentPage = 0;
-  final List<Widget> _pages = [HomePage(), ServicePage()];
+  final List<Widget> _pages = [HomePage(), ServicePage(),RobotPage(),MessagePage(),MyPage()];
 
   @override
   Widget build(BuildContext context) {
@@ -22,15 +25,22 @@ class TabBarPageState extends State<TabBarPage> {
       ),
       body: _pages[_currentPage],
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(
-          Icons.add,
-          color: Colors.white,
-        ),
+        onPressed: () {
+          setState(() {
+            this._currentPage = 2;
+          });
+        },
+        child: Image.asset("assets/images/tab_bar/robot.png", width: 34, height: 60),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        highlightElevation: 0,
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: BottomNavigationBar(
         showSelectedLabels: true,
+        unselectedFontSize: 10,
+        selectedFontSize: 10,
+        selectedItemColor: Colors.orange,
         type: BottomNavigationBarType.fixed,
         currentIndex: _currentPage,
         onTap: (int index) {
@@ -54,22 +64,22 @@ class TabBarPageState extends State<TabBarPage> {
             label: "服务",
           ),
           BottomNavigationBarItem(
-            icon: Image.asset("assets/images/tab_bar/home.png", width: 20, height: 20),
-            activeIcon: Image.asset("assets/images/tab_bar/home_selected.png",
+            icon: Image.asset("assets/images/tab_bar/robot.png", width: 20, height: 20),
+            activeIcon: Image.asset("assets/images/tab_bar/robot.png",
                 width: 20, height: 20),
             label: "",
           ),
           BottomNavigationBarItem(
-            icon: Image.asset("assets/images/tab_bar/home.png", width: 20, height: 20),
-            activeIcon: Image.asset("assets/images/tab_bar/home_selected.png",
+            icon: Image.asset("assets/images/tab_bar/message.png", width: 20, height: 20),
+            activeIcon: Image.asset("assets/images/tab_bar/message_selected.png",
                 width: 20, height: 20),
-            label: "首页",
+            label: "消息",
           ),
           BottomNavigationBarItem(
-            icon: Image.asset("assets/images/tab_bar/home.png", width: 20, height: 20),
-            activeIcon: Image.asset("assets/images/tab_bar/home_selected.png",
+            icon: Image.asset("assets/images/tab_bar/my.png", width: 20, height: 20),
+            activeIcon: Image.asset("assets/images/tab_bar/my_selected.png",
                 width: 20, height: 20),
-            label: "首页",
+            label: "我的",
           ),
         ],
       ),
