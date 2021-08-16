34c9d52fa4070eb0eb36b246ff450005cf73d1a6
yangzhuang <koolss@koolss.com>
2021-08-17 00:27:32 +0800

    tabBar 凸出部分

--- lib/page/tab_bar/TabBarPage.dart
+++ lib/page/tab_bar/TabBarPage.dart
@@ -4,16 +4,16 @@ import 'package:flutter/material.dart';
 import 'package:xiaowu/page/home/HomePage.dart';
 import 'package:xiaowu/page/service/ServicePage.dart';
 
-class TabBarPage extends StatefulWidget{
-
+class TabBarPage extends StatefulWidget {
   @override
-  State<StatefulWidget> createState()=>TabBarPageState();
+  State<StatefulWidget> createState() => TabBarPageState();
 }
 
-class TabBarPageState extends State<TabBarPage>{
+class TabBarPageState extends State<TabBarPage> {
   // 当前页面标识
   int _currentPage = 0;
-  final List<Widget> _pages = [HomePage(),ServicePage()];
+  final List<Widget> _pages = [HomePage(), ServicePage()];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,26 +21,56 @@ class TabBarPageState extends State<TabBarPage>{
         title: Text("TabBar"),
       ),
       body: _pages[_currentPage],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: BottomNavigationBar(
         showSelectedLabels: true,
         type: BottomNavigationBarType.fixed,
         currentIndex: _currentPage,
-        onTap: (int index){
+        onTap: (int index) {
           setState(() {
             this._currentPage = index;
           });
         },
         items: [
           BottomNavigationBarItem(
-            icon: Image.asset("images/tab_bar/home.png",width: 20,height: 20),
-            activeIcon: Image.asset("images/tab_bar/home_selected.png",width: 20,height: 20),
-            label:"首页",
+            icon: Image.asset("assets/images/tab_bar/home.png", width: 20, height: 20),
+            activeIcon: Image.asset("assets/images/tab_bar/home_selected.png",
+                width: 20, height: 20),
+            label: "首页",
+          ),
+
+          BottomNavigationBarItem(
+            icon: Image.asset("assets/images/tab_bar/service.png",
+                width: 20, height: 20),
+            activeIcon: Image.asset("assets/images/tab_bar/service_selected.png",
+                width: 20, height: 20),
+            label: "服务",
           ),
           BottomNavigationBarItem(
-            icon: Image.asset("images/tab_bar/service.png",width: 20,height: 20),
-            activeIcon: Image.asset("images/tab_bar/service_selected.png",width: 20,height: 20),
-            label: "服务",
-          )
+            icon: Image.asset("assets/images/tab_bar/home.png", width: 20, height: 20),
+            activeIcon: Image.asset("assets/images/tab_bar/home_selected.png",
+                width: 20, height: 20),
+            label: "",
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset("assets/images/tab_bar/home.png", width: 20, height: 20),
+            activeIcon: Image.asset("assets/images/tab_bar/home_selected.png",
+                width: 20, height: 20),
+            label: "首页",
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset("assets/images/tab_bar/home.png", width: 20, height: 20),
+            activeIcon: Image.asset("assets/images/tab_bar/home_selected.png",
+                width: 20, height: 20),
+            label: "首页",
+          ),
         ],
       ),
     );
