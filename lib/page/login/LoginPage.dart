74991e9d9ea20a117ae64925975f800cefdfadba
yangzhuang <koolss@koolss.com>
2021-08-17 20:52:15 +0800

    完成登录页面

--- lib/page/login/LoginPage.dart
+++ lib/page/login/LoginPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:xiaowu/util/ColorUtil.dart';
 
 class LoginPage extends StatefulWidget {
@@ -11,198 +12,230 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   TextEditingController phoneController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body:SingleChildScrollView(
-          child: GestureDetector(
-            behavior: HitTestBehavior.translucent,
-            onTap: () {
-              // 触摸收起键盘
-              FocusScope.of(context).requestFocus(FocusNode());
-            },
-            child: Container(
-                decoration: BoxDecoration(
-                  image: new DecorationImage(
-                    fit: BoxFit.fill,
-                    image: AssetImage("assets/images/login/bg.png"),
+    ScreenUtil.instance = ScreenUtil(width:750,height:1334)..init(context);
+
+    Widget logoSection = Container(
+      child: Image.asset(
+        "assets/images/login/logo.png",
+        width: 130,
+        height: 130,
+      ),
+    );
+
+    Widget labelSection = Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            child: Text(
+              "手机验证码登录",
+              style: TextStyle(
+                  fontSize: 24, color: ColorUtil.fromHex('#FF161833')),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 16),
+            child: Text(
+              "未注册手机可输入验证码完成注册",
+              style: TextStyle(
+                  fontSize: 14, color: ColorUtil.fromHex('#FF94969E')),
+            ),
+          ),
+        ],
+      ),
+    );
+
+    Widget phoneSection = Container(
+
+      child: TextField(
+        controller: phoneController,
+        keyboardType: TextInputType.number,
+        decoration: InputDecoration(
+          hintText: "请输入手机号码",
+          hintStyle: TextStyle(
+            color: ColorUtil.fromHex("#FF94969E"),
+          ),
+          prefixIcon: Container(
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  child: Text(
+                    "+86",
+                    style: TextStyle(
+                        color: ColorUtil.fromHex("#161833"), fontSize: 16),
                   ),
                 ),
-                child: Container(
-                  margin: EdgeInsets.symmetric(vertical: 0, horizontal: 30),
-                  child: Column(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.only(top: 96),
-                        child: Image.asset(
-                          "assets/images/login/logo.png",
-                          width: 130,
-                          height: 130,
-                        ),
-                      ),
-                      Container(
-                        alignment: Alignment(-1, 0),
-                        margin: EdgeInsets.only(top: 30),
-                        child: Text(
-                          "手机验证码登录",
-                          style: TextStyle(
-                              fontSize: 24, color: ColorUtil.fromHex('#FF161833')),
-                        ),
-                      ),
-                      Container(
-                        alignment: Alignment(-1, 0),
-                        margin: EdgeInsets.only(top: 16),
-                        child: Text(
-                          "未注册手机可输入验证码完成注册",
-                          style: TextStyle(
-                              fontSize: 14, color: ColorUtil.fromHex('#FF94969E')),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(top: 34),
-                        child: TextField(
-                          controller: phoneController,
-                          keyboardType: TextInputType.number,
-                          decoration: InputDecoration(
-                            hintText: "请输入手机号码",
-                            hintStyle: TextStyle(
-                              color: ColorUtil.fromHex("#FF94969E"),
-                            ),
-                            prefixIcon: Container(
-                              child: Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  Container(
-                                    child: Text(
-                                      "+86",
-                                      style: TextStyle(
-                                          color: ColorUtil.fromHex("#161833"),
-                                          fontSize: 16),
-                                    ),
-                                  ),
-                                  Container(
-                                    margin: EdgeInsets.only(left: 9),
-                                    child: Icon(
-                                      Icons.keyboard_arrow_down_sharp,
-                                      color: ColorUtil.fromHex("#94969E"),
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ),
-                            border: UnderlineInputBorder(
-                              borderSide:
-                              BorderSide(color: ColorUtil.fromHex("#DDDEDD")),
-                            ),
-                            focusedBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(
-                                    color: ColorUtil.fromHex("#DDDEDD"))),
-                            suffixIcon: GestureDetector(
-                              onTap: (){
-                                this.phoneController.clear();
-                              },
-                              child: Image.asset("assets/images/login/cha.png"),
-                            ),
-                          ),
-                          autofocus: false,
-                        ),
-                      ),
-                      Opacity(
-                        opacity: this.checkPhone()?1:0.5,
-                        child: Container(
-                          margin: EdgeInsets.only(top: 39),
-                          width: MediaQuery.of(context).size.width,
-                          decoration: BoxDecoration(
-                              gradient: LinearGradient(
-                                colors: [
-                                  ColorUtil.fromHex("#F76600"),
-                                  ColorUtil.fromHex("#FCA000")
-                                ],
-                              ),
-                              borderRadius: BorderRadius.circular(24.5)),
-                          child: TextButton(
-                            child: Text(
-                              '获取验证码',
-                              style: TextStyle(color: Colors.white),
-                            ),
-                            onPressed: ()=>this.checkPhone()?this.getVerificationCode():null,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(bottom: 16),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Container(
-                              decoration: BoxDecoration(
-                                  gradient: LinearGradient(
-                                    colors: [
-                                      ColorUtil.fromHex("#FFFFFF"),
-                                      ColorUtil.fromHex("#94969E")
-                                    ],
-                                  ),
-                              ),
-                              child: Container(width: 50, height: 1,),
-                            ),
-                            Container(
-                              margin: EdgeInsets.only(left: 10),
-                              child: Text("使用第三方登录",style: TextStyle(color: ColorUtil.fromHex("#93959B"),fontSize: 14),),
-                            ),
-                            Container(
-                              margin: EdgeInsets.only(left: 10),
-                              decoration: BoxDecoration(
-                                gradient: LinearGradient(
-                                  colors: [
-                                    ColorUtil.fromHex("#94969E"),
-                                    ColorUtil.fromHex("#FFFFFF")
-                                  ],
-                                ),
-                              ),
-                              child: Container(width: 50, height: 1,),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(bottom: 50),
-                        child: GestureDetector(
-                          onTap: (){
-                            this.weiXinLogin();
-                          },
-                          child: Image.asset(
-                            "assets/images/login/weixin.png",
-                            width: 32,
-                            height: 32,
-                          ),
-                        ),
-                      ),
-                    ],
+                Container(
+                  margin: EdgeInsets.only(left: 9),
+                  child: Icon(
+                    Icons.keyboard_arrow_down_sharp,
+                    color: ColorUtil.fromHex("#94969E"),
                   ),
-                )),
+                )
+              ],
+            ),
           ),
+          border: UnderlineInputBorder(
+            borderSide: BorderSide(color: ColorUtil.fromHex("#DDDEDD")),
+          ),
+          focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: ColorUtil.fromHex("#DDDEDD"))),
+          suffixIcon: GestureDetector(
+            onTap: () {
+              this.phoneController.clear();
+            },
+            child: Image.asset("assets/images/login/cha.png"),
+          ),
+        ),
+        autofocus: false,
+      ),
+    );
+
+    Widget btnSection = Opacity(
+      opacity: this.checkPhone() ? 1 : 0.5,
+      child: Container(
+        margin: EdgeInsets.only(top: 39),
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                ColorUtil.fromHex("#F76600"),
+                ColorUtil.fromHex("#FCA000")
+              ],
+            ),
+            borderRadius: BorderRadius.circular(24.5)),
+        child: TextButton(
+          child: Text(
+            '获取验证码',
+            style: TextStyle(color: Colors.white),
+          ),
+          onPressed: () =>
+              this.checkPhone() ? this.getVerificationCode() : null,
+        ),
+      ),
+    );
+
+    Widget weiXinSection = Container(
+      child: Column(
+        children: [
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        ColorUtil.fromHex("#FFFFFF"),
+                        ColorUtil.fromHex("#94969E")
+                      ],
+                    ),
+                  ),
+                  child: Container(
+                    width: 50,
+                    height: 1,
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 10),
+                  child: Text(
+                    "使用第三方登录",
+                    style: TextStyle(
+                        color: ColorUtil.fromHex("#93959B"), fontSize: 14),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 10),
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        ColorUtil.fromHex("#94969E"),
+                        ColorUtil.fromHex("#FFFFFF")
+                      ],
+                    ),
+                  ),
+                  child: Container(
+                    width: 50,
+                    height: 1,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            child: GestureDetector(
+              onTap: () {
+                this.weiXinLogin();
+              },
+              child: Image.asset(
+                "assets/images/login/weixin.png",
+                width: 32,
+                height: 32,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+
+    return Scaffold(
+        body:GestureDetector(
+          behavior: HitTestBehavior.translucent,
+          onTap: () {
+            // 触摸收起键盘
+            FocusScope.of(context).requestFocus(FocusNode());
+          },
+          child: Container(
+              decoration: BoxDecoration(
+                image: new DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage("assets/images/login/bg.png"),
+                ),
+              ),
+              child: Container(
+                margin: EdgeInsets.symmetric(vertical: 0,horizontal: 30),
+                child: new ListView(
+                  children: <Widget>[
+                    new SizedBox(height: ScreenUtil().setHeight(96),),
+                    logoSection,
+                    new SizedBox(height: ScreenUtil().setHeight(30),),
+                    labelSection,
+                    new SizedBox(height: ScreenUtil().setHeight(45),),
+                    phoneSection,
+                    new SizedBox(height: ScreenUtil().setHeight(40),),
+                    btnSection,
+                    new SizedBox(height: ScreenUtil().setHeight(216),),
+                    weiXinSection,
+                    new SizedBox(height: ScreenUtil().setHeight(53),),
+                  ],
+                )),
+              )
         )
     );
   }
 
   /// 校验电话号码是否通过
-  bool checkPhone(){
+  bool checkPhone() {
     var phone = this.phoneController.text;
-    if(phone.isEmpty){
+    if (phone.isEmpty) {
       return false;
     }
-    if(phone.length != 11){
+    if (phone.length != 11) {
       return false;
     }
     return true;
   }
 
-  bool getVerificationCode(){
+  bool getVerificationCode() {
     print("获取验证码");
     return true;
   }
 
-  bool weiXinLogin(){
+  bool weiXinLogin() {
     print("点击微信登录");
     return true;
   }
